5c5379b633f7a6796083aa540ca927d44af23668
elywin <lukeeraelywin@gmail.com>
2020-10-22 05:23:45 +0300

    ft(add:post questions)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import PostQuestion from './components/posts';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <PostQuestion />
     </div>
   );
 }
